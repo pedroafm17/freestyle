fdd824a171d8bdaff4acd51974f31601b4e850ca
Pedro Aires Fischer Maciel <20241144010018@ifrn.local>
2026-05-21 15:27:17 -0300

    Atualização n1 do contador de dias

---
+++ contador.js
@@ -0,0 +1,23 @@
+function attdata(){
+    const datatual = new Date();
+    const dataalvo = new Date("2026-07-22");
+    const local = document.getElementById("dias");
+
+    const diferenca = dataalvo - datatual;
+    const diasfaltando = Math.floor(diferenca/(1000* 60 * 60 *24));
+    local.textContent = `${diasfaltando+1}`
+}
+
+function attdata2(){
+    const datatual = new Date();
+    const dataalvo = new Date("2026-12-24");
+    const local = document.getElementById("dias2")
+
+    const diferenca = dataalvo-datatual;
+    const diasfaltando = Math.floor(diferenca/(1000*60*60*24))
+
+    local.textContent = `${diasfaltando+1}`;
+}
+
+attdata();
+attdata2();
