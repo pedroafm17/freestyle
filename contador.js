24edc78c43672833c7c7b430f656b180079a71fe
Pedro Aires Fischer Maciel <20241144010018@ifrn.local>
2026-06-08 15:56:03 -0300

    Adição de novas abas futuras

--- contador.js
+++ contador.js
@@ -19,5 +19,17 @@ function attdata2(){
     local.textContent = `${diasfaltando+1}`;
 }
 
+function attdata3(){
+    const datatual = new Date();
+    const dataalvo = new Date("2026-12-18");
+    const local = document.getElementById("dias3")
+
+    const diferenca = dataalvo-datatual;
+    const diasfaltando = Math.floor(diferenca/(1000*60*60*24))
+
+    local.textContent = `${diasfaltando+1}`;
+}
+
 attdata();
 attdata2();
+attdata3();
